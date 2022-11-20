61a932997be66279d2677701fa22aa5e358a9671
vorago24 <isaias.raul.ruiz@gmail.com>
2022-11-20 11:24:29 -0800

    deleting model files

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -8,10 +8,10 @@ const bcrypt = require('bcrypt');
 
 const handleNewUser = async (req, res) => {
     const { user, pwd } = req.body;
-    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.'});
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
     const duplicate = usersDB.users.find(person => person.username === user);
-    if(duplicate) return res.sendStatus(409);
-    try{
+    if (duplicate) return res.sendStatus(409);
+    try {
         //encrypt the password
         const hashedPwd = await bcrypt.hash(pwd, 10);
         //store the new user
@@ -19,7 +19,7 @@ const handleNewUser = async (req, res) => {
         usersDB.setUsers([...usersDB.users, newUser]);
         await fsPromises.writeFile(path.join(__dirname, '..', 'model', 'users.json'), JSON.stringify(usersDB.users));
         console.log(usersDB.users)
-        res.status(201).json({ 'success': `New user ${user} created!`})
+        res.status(201).json({ 'success': `New user ${user} created!` })
     } catch (err) {
         res.status(500).json({ 'message': err.message });
     }
